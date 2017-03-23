e3d3fbdd34aefaa172e6d8dd203639df1db6db0f
Jozko Skrablin <jozko@zomg.si>
2017-03-23 17:55:54 +0100

    Adding pinned nixpkgs sample and nix-shell script sample

---
+++ 05-PinPyRedis/default.nix
@@ -0,0 +1,48 @@
+with import (fetchTarball https://nixos.org/channels/nixos-17.03/nixexprs.tar.xz) {};
+
+let
+    redis30 = pkgs.redis.overrideDerivation (super: {
+    name = "redis30";
+    src = fetchurl {
+       url = "http://download.redis.io/releases/redis-3.0.7.tar.gz";
+       sha256 = "b2a791c4ea3bb7268795c45c6321ea5abcc24457178373e6a6e3be6372737f23";
+         };
+       });
+
+     pyredis = python27Packages.buildPythonPackage rec {
+         name = "pyredis";
+         src = fetchurl {
+           url = "https://pypi.python.org/packages/68/44/5efe9e98ad83ef5b742ce62a15bea609ed5a0d1caf35b79257ddb324031a/redis-2.10.5.tar.gz";
+           sha256 = "5dfbae6acfc54edf0a7a415b99e0b21c0a3c27a7f787b292eea727b1facc5533";
+           };
+       propagatedBuildInputs = [ redis30 ];
+       checkPhase = ''
+       '';
+       doCheck = false;
+       dontStrip = true;
+       };
+in
+
+stdenv.mkDerivation {
+  name = "OverrideRedis";
+  buildInputs = [
+     git
+     redis30
+     python27Full
+     python27Packages.virtualenv
+     pyredis
+     ];
+      src = null;
+      # When used as `nix-shell --pure`
+      shellHook = ''
+      export SOURCE_DATE_EPOCH=315532800
+      unset http_proxy
+      export GIT_SSL_CAINFO=/etc/ssl/certs/ca-bundle.crt
+      '';
+      # used when building environments
+      extraCmds = ''
+      unset http_proxy # otherwise downloads will fail ("nodtd.invalid")
+      export SOURCE_DATE_EPOCH=315532800
+      export GIT_SSL_CAINFO=/etc/ssl/certs/ca-bundle.crt
+      '';
+    }
